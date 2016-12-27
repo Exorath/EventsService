8f80b8c9c81e35f1999608ac0c1eb82a68704f8a
Toon Sevrin <twan123@live.be>
2016-12-27 16:02:20 +0100

    Objectifying the subscribe request within the socket

---
+++ src/main/java/com/exorathcloud/service/events/res/SubscribeRequest.java
@@ -0,0 +1,35 @@
+package com.exorathcloud.service.events.res;
+
+/**
+ * Created by toonsev on 12/27/2016.
+ */
+public class SubscribeRequest {
+    private String[] events;
+    private String serviceId;
+    private String instanceId;
+
+
+    /**
+     * Gets a list of events this request likes to subscribe to
+     * @return a list of events to subscribe to
+     */
+    public String[] getEvents() {
+        return events;
+    }
+
+    /**
+     * Gets the serviceId of the requesting service (this is an id that is unique per service, not per service instance!)
+     * @return the serviceId of the requester
+     */
+    public String getServiceId() {
+        return serviceId;
+    }
+
+    /**
+     * Gets an instanceId of the requesting service, this is unique per service instance.
+     * @return the instanceId of the requester
+     */
+    public String getInstanceId() {
+        return instanceId;
+    }
+}
